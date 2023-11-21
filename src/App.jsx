84396ab6b744b08111ca68a5094bfd879b13d260
Aseem Panigrahi <Aseem.Panigrahi@ibm.com>
2023-11-21 20:54:04 +0530

    removed default code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Button } from '@mui/material';
 
 function App() {
 
   return (
     <>
-        <p>Task management app</p>
+        <Button variant='contained'>Add Task</Button>
     </>
   )
 }
